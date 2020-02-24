443d03e27eb6be2cd5ba9a72526c2a0ca8681f43
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-02-24 12:17:07 -0500

    Rename to Commercial_Controller

---
+++ Commercial_Controller_CSharp/Commercial_Controller.cs
@@ -0,0 +1,22 @@
+﻿///////////////////////////////////////////////////////
+// {Commercial Controller}                          //
+///////////////////////////////////////////////////////
+// Author: {Joshua Knutson}                          //
+// License: {GNUGPLv3}                               //
+// Link: {https://www.gnu.org/licenses/gpl-3.0.html} //
+// Version: {1.00}                                   //
+// Contact: {github.com/alberbecois}                 //
+///////////////////////////////////////////////////////
+
+using System;
+
+namespace Commercial_Controller_CSharp
+{
+    class Commercial_Controller
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
