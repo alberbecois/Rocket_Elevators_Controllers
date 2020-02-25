358412fb400d0c17944b2f53970e3eee8855dc20
Joshua Knutson <37628377+alberbecois@users.noreply.github.com>
2020-02-25 17:26:56 -0500

    Update Commercial_Controller.go

--- Commercial_Controller_Go/Commercial_Controller.go
+++ Commercial_Controller_Go/Commercial_Controller.go
@@ -10,8 +10,64 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+//////////////////
+// Cage Manager //
+//////////////////
+
+type CageManager struct {
+}
+
+//////////////////////////
+// System Configuration //
+//////////////////////////
+
+type Configuration struct {
+	batteryOn      bool
+	totalColumns   int
+	cagesPerColumn int
+	totalFloors    int
+	totalBasements int
+}
+
+func askForConfirmation() bool {
+	var response string
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	okayResponses := []string{"y", "Y", "yes", "Yes", "YES"}
+	nokayResponses := []string{"n", "N", "no", "No", "NO"}
+	if containsString(okayResponses, response) {
+		return true
+	} else if containsString(nokayResponses, response) {
+		return false
+	} else {
+		fmt.Println("Please type yes or no and then press enter:")
+		return askForConfirmation()
+	}
+}
+
+//////////
+// Main //
+//////////
 
 func main() {
-	fmt.Println("Hello, 世界")
+	fmt.Println("Hello, 世界 - お名前は？")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println("Hello, " + input)
+
+	fmt.Println("Activate battery? [y/n]")
+
+	if askForConfirmation() == true {
+		fmt.Println("Initializing...")
+	} else {
+		fmt.Println("Startup aborted!")
+	}
+
 }
